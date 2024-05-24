30eae6a1b4c5a50053a330da5622d433f47c6310
incubator4 <aries0robin@gmail.com>
2024-05-25 00:04:52 +0800

    chore: Remove unused useRouter import in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 import { Snippet } from "@nextui-org/snippet";
-import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="mt-8">
