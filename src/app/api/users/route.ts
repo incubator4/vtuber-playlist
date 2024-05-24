1342c2cd88ca351e91e73b07d9f5c88a85282358
incubator4 <aries0robin@gmail.com>
2024-05-25 00:22:13 +0800

    Update users route to fetch and return user IDs

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,13 +1,8 @@
-"use server";
 import { NextResponse } from "next/server";
 import { getUsers } from "@/lib/notion";
 
 export async function GET(_request: Request) {
   const users = await getUsers();
-
-  const resp = users.map((user) => ({
-    id: user.id,
-  }));
 
   return NextResponse.json(users);
 }
