a9545590375868271f21a8e03322b96b72af55fc
incubator4 <aries0robin@gmail.com>
2024-05-25 00:18:33 +0800

    feat: Update users route to fetch and return user IDs

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,14 +1,15 @@
 "use server";
 import { NextResponse } from "next/server";
-// import { getUsers } from "@/lib/notion";
+import { getUsers } from "@/lib/notion";
 
 export async function GET(_request: Request) {
-  // const users = await getUsers();
+  const users = await getUsers();
 
-  // const resp = users.map((user) => ({
-  //   id: user.id,
-  // }));
-  return NextResponse.json([]);
+  const resp = users.map((user) => ({
+    id: user.id,
+  }));
 
-  // return NextResponse.json(users);
+  return NextResponse.json(users);
 }
+
+export const dynamic = "force-dynamic";
