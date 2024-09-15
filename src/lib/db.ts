74bf31829946e7194903f82981720fcfe8d08e76
incubator4 <aries0robin@gmail.com>
2024-09-15 19:49:18 +0800

    Refactor environment variable handling in db.ts and introduce env.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,11 +3,10 @@ import { Pool } from "@neondatabase/serverless";
 import * as schema from "@/drizzle/schema";
 import * as relations from "@/drizzle/relations";
 
-console.log(process.env);
-console.log(process.env.DATABASE_URL);
+import { env } from "./env";
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: env.DATABASE_URL,
 });
 
 export const db = drizzle(pool, {
