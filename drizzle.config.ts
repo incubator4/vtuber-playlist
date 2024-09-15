0e7e5742919fa794144d657c007430b244bd12d7
incubator4 <aries0robin@gmail.com>
2024-09-15 19:32:37 +0800

    Refactor drizzle.config.ts to log environment variables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,9 @@ import { createEnv } from "@t3-oss/env-core";
 import { defineConfig } from "drizzle-kit";
 import { z } from "zod";
 
+console.log(process.env);
+console.log(process.env.DATABASE_URL);
+
 const env = createEnv({
   server: {
     DATABASE_URL: z.string().url().min(1),
