43dc7c00cdf12c8ea872f4c73fb3352196105f97
incubator4 <aries0robin@gmail.com>
2024-09-15 19:41:48 +0800

    Refactor environment variable in .env.example and drizzle.config.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,6 +3,9 @@ import { Pool } from "@neondatabase/serverless";
 import * as schema from "@/drizzle/schema";
 import * as relations from "@/drizzle/relations";
 
+console.log(process.env);
+console.log(process.env.DATABASE_URL);
+
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
